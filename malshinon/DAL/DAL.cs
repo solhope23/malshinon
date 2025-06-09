7c65c0570633c58aafacef4cb9230f6d51d8588d
solhope23 <s526413103@gmail.com>
2025-06-09 22:46:38 +0300

    קונסטרקטור

---
+++ malshinon/DAL/DAL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+public class DAL
+{
+    private string connStr;
+    private MySqlConnection DataBase;
+
+    public DAL(string server = "localhost", string username = "root", string password = "", string database = "malshinon")
+    {
+        connStr = $"server={server};username={username};password={password};database={database}";
+
+        this.DataBase = new MySqlConnection(connStr);
+
+        //this.TestConnection();
+    }
+
+}
